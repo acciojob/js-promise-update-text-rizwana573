32c163000a6a8924a43d32abe7800265633b4146
rizwana573 <95849132+rizwana573@users.noreply.github.com>
2023-10-30 17:43:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const promiseFn(){
+	return new Promise((resolve, reject) => {
+	setTimeout(()=>{
+		resolve("Hello World!");
+	},1000);
+ });
+} 
+const output = ocument.getElementById('output');
+
+promiseFn()
+	.then((message)=>{
+		output.innerText = message;
+	})
+    .catch((error)=>{
+		console.error("An error occured", error);
+	});
